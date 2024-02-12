34a57682200b4466078bd322affbbf28fbc4072a
Felipe <u202110902@upc.edu.pe>
2024-02-12 11:49:29 -0500

    test

---
+++ src/Test/main.cpp
@@ -0,0 +1,18 @@
+// #include <bits/stdc++.h>
+typedef long long ll;
+
+using namespace std;
+
+void solve(int abc) {}
+
+int main()
+{
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(nullptr);
+	
+	int t = 1;
+	// cin>>t;
+	while (t--)
+		solve(123);
+	return 0;
+}
